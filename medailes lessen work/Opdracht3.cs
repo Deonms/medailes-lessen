ef2b9ce9c0728ea39e4e1baca46edea123bf375f
DeonsGaming <Deon.mob@gmail.com>
2025-11-17 11:39:49 +0100

    Update Opdracht3.cs

--- medailes lessen work/Opdracht3.cs
+++ medailes lessen work/Opdracht3.cs
@@ -13,10 +13,18 @@ namespace medailes_lessen_work
             Console.WriteLine("Wat voor weer is het Vandaag?: 1. Zon 2. Regen 3.Bewolkt. 4.Sneeuw 5.Iets anders");
             String Weer = Console.ReadLine();
             Console.WriteLine("en hoe warm is het vandaag?");
-            String Graden = Console.ReadLine();
+            float Graden = Console.ReadLine();
             if (Weer == "5")
             {
                 Console.WriteLine("Sorry ik kan je dan helaas niet helpen");
+            }
+            if (Weer == "1" && Graden >= 15)
+            {
+                Console.WriteLine("Het is Dus zonnig en het is " + Graden + " Graden ik raad aan om je intesmeren voordat jij je verbrant");
+            }
+            else if (Weer == "1" && Graden < 15)
+            {
+                Console.WriteLine("het is zonnig maar de graden zou er niet perse hoeven te zorgen dat je verbrand maar indien je wilt zou je jezelf kunnen insmeren");
             }
         }
     }
