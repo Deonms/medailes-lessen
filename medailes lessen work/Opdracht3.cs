3093a377605f3945d1208d11fcb30770487859fe
DeonsGaming <Deon.mob@gmail.com>
2025-11-17 11:32:40 +0100

    Update Opdracht3.cs

--- medailes lessen work/Opdracht3.cs
+++ medailes lessen work/Opdracht3.cs
@@ -10,7 +10,14 @@ namespace medailes_lessen_work
     {
         public void Start() 
         {
-             
+            Console.WriteLine("Wat voor weer is het Vandaag?: 1. Zon 2. Regen 3.Bewolkt. 4.Sneeuw 5.Iets anders");
+            String Weer = Console.ReadLine();
+            Console.WriteLine("en hoe warm is het vandaag?");
+            String Graden = Console.ReadLine();
+            if (Weer == "5")
+            {
+                Console.WriteLine("Sorry ik kan je dan helaas niet helpen");
+            }
         }
     }
 }
