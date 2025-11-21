d216fb763512ef5e80a47240cefcf4e4c84ebfcf
DeonsGaming <Deon.mob@gmail.com>
2025-11-21 11:17:59 +0100

    Create opdracht in de les.cs

---
+++ medailes lessen work/opdracht in de les.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medailes_lessen_work
+{
+    internal class opdracht_in_de_les
+    {
+        public void Start()
+        {
+            Console.WriteLine("Geef me een getal");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            if (getal > 0)
+            {
+
+            }
+        }
+    }
+}
