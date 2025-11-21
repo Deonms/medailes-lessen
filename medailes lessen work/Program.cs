6cfe5a8334af3804652477df80b39b223fad1f8d
DeonsGaming <Deon.mob@gmail.com>
2025-11-21 10:52:07 +0100

    Update Program.cs

--- medailes lessen work/Program.cs
+++ medailes lessen work/Program.cs
@@ -11,7 +11,7 @@ namespace medailes_lessen_work
     {
         static void Main(string[] args)
         {
-            
+            /*
             Console.WriteLine("Wat is je naam en je leeftijd?");
             string naam = Console.ReadLine();
             string leeftijd = Console.ReadLine();
@@ -23,7 +23,7 @@ namespace medailes_lessen_work
 
             Opdracht3 Opdracht3 = new Opdracht3();
             Opdracht3.Start();
-            
+            */
         }
     }
 }
