acdabafdec22bc12d0709ed56de204ebe2be75ee
DeonsGaming <Deon.mob@gmail.com>
2025-12-01 11:12:50 +0100

    Create Opdracht5.cs

---
+++ medailes lessen work/Opdracht5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medailes_lessen_work
+{
+    internal class Opdracht5
+    {
+        public void Start()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
