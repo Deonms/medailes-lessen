d94ac9bce2195c21756012ab479d02dd5fc741ec
DeonsGaming <Deon.mob@gmail.com>
2025-12-03 08:57:35 +0100

    Update Opdracht5.cs

--- medailes lessen work/Opdracht5.cs
+++ medailes lessen work/Opdracht5.cs
@@ -11,16 +11,25 @@ namespace medailes_lessen_work
         public void Start()
         {
             Console.WriteLine("Wat wil je keer doen?");
-            int getalkeer = Convert.ToInt32(Console.ReadLine());
+            int getalkeer;
+            while (!int.TryParse(Console.ReadLine(), out getalkeer))
+            {
+                Console.WriteLine("Dit is geen cijfer");
+            }
+
             Console.WriteLine("en hoe vaak wil je het keer doen? (hoe groter de nummer hoe langer het duurt)");
-            int maximumgetal = Convert.ToInt32(Console.ReadLine());
+            int maximumgetal;
+            while (!int.TryParse(Console.ReadLine(), out maximumgetal))
+            {
+                Console.WriteLine("Dit is geen cijfer");
+            }
+
 
             for (int i = 0; i <= maximumgetal; i++)
             {
                 Console.WriteLine(getalkeer + " x " + i + " = " + getalkeer * i);
             }
-            
-            
+           
         }
     }
 }
