2817f8d7e7617e5627692dd864f1aab7622fc26e
DeonsGaming <Deon.mob@gmail.com>
2025-12-03 10:26:41 +0100

    Update Opdracht6.cs

--- medailes lessen work/Opdracht6.cs
+++ medailes lessen work/Opdracht6.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,28 @@ namespace medailes_lessen_work
     {
         public void Start()
         {
-            int numbergeusser = 3478;
+            int i = 0;
+            Console.WriteLine("geeft een cijfer");
+            while (!int.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine("Dit is geen cijfer");
+            }
+
+            int imax = 17;
+            int imin = 0;
+            int[] numberguesserarray = { 54, 3734, 8734, 1, 35, -436, 9456, 287, 6454, 6782, 729, 392, -39573, 25, 2009, 38, -2009 };
+            int numberguesser = numberguesserarray[i];
+            for (i > imax)
+            {
+                if (i > imax)
+                {
+                    i = i / 10;
+                }
+            }
+                if (i < imin)
+            {
+                i = i * imin;
+            }
             bool running = true;
             int maximum1 = 0;
             int tries = 0;
@@ -29,17 +51,17 @@ namespace medailes_lessen_work
                     Console.WriteLine($"Dit is geen cijfer, dit kost je ook een try {tries}");
                 }
                 Console.Clear();
-                if (answer == numbergeusser)
+                if (answer == numberguesser)
                 {
                     running = false;
                     Console.WriteLine($"Je hebt gewonnen in {tries} beurten");
                 }
-                if (answer > numbergeusser)
+                if (answer > numberguesser)
                 {
                     tries = tries + 1;
                     Console.WriteLine($"Lager, beurten al gedaan {tries}");
                 }
-                else if (answer < numbergeusser)
+                else if (answer < numberguesser)
                 {
                     tries = tries + 1;
                     Console.WriteLine($"Hoger, beurten al gedaan {tries}");
