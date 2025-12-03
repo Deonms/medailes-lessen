ba06dac806e1be8cc8e117b0e9854fad9a28ab60
DeonsGaming <Deon.mob@gmail.com>
2025-12-03 09:15:03 +0100

    User and Modification
    created a user class and modified the game hoger lager

--- medailes lessen work/Opdracht6.cs
+++ medailes lessen work/Opdracht6.cs
@@ -10,17 +10,34 @@ namespace medailes_lessen_work
     {
         public void Start()
         {
+            int getalraad = 6;
             bool running = true;
+            int maximum1 = 0;
+            int tries = 0;
             while (running)
             {
-                Console.WriteLine("Wat is jouw Naam?");
-                string answer = Console.ReadLine();
-
-                if (answer == "Deon")
+                if (maximum1 == 0)
+                {
+                    Console.WriteLine("Raad het getal");
+                    maximum1 = maximum1 + 1;
+                }
+                int answer = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (answer == 6)
                 {
                     running = false;
+                    Console.WriteLine($"Je hebt gewonnen in {tries} beurten");
                 }
-                Console.Clear();
+                if (answer > getalraad)
+                {
+                    tries = tries + 1;
+                    Console.WriteLine($"Lager, beurten al gedaan {tries}");
+                }
+                else if (answer < getalraad)
+                {
+                    tries = tries + 1;
+                    Console.WriteLine($"Hoger, beurten al gedaan {tries}");
+                }
             }
         }
     }
---
+++ medailes lessen work/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medailes_lessen_work
+{
+    internal class User
+    {
+    }
+}
